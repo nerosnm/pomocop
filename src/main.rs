0a262459c683275f1a52bfacc135f1a15be9c205
Søren Mortensen <soren@neros.dev>
2022-03-09 23:13:09 +0000

    Remove old commented code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,6 @@ async fn main() -> Result<(), pomocop::Error> {
     tracing_subscriber::fmt::init();
     dotenv::dotenv().ok();
 
-    // let mess_with = tokio::spawn(
-    //     async move {
-    //         sleep(Duration::from_secs(2)).await;
-
-    //         info!("sending skip message");
-    //         session_2.lock().unwrap().skip().expect("failed to skip");
-
-    //         sleep(Duration::from_secs(62)).await;
-
-    //         info!("sending stop message");
-    //         session_2.lock().unwrap().stop().expect("failed to stop");
-    //     }
-    //     .instrument(tracing::info_span!("mess_with", ?id)),
-    // );
-
     pomocop::run(
         var("APPLICATION_ID")?,
         var("OWNER_ID")?,
